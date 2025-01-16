50cafef1a370afe858576a8d4ba4d9539d345486
Ivan <stepanovivan548@gmail.com>
2025-01-16 21:44:27 +0700

    Типы данных

---
+++ Data types/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_types
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+        }
+    }
+}
