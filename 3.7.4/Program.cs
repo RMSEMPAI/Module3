6eed76d3f4256559affb01f5b6074c64cb196b76
Ivan <stepanovivan548@gmail.com>
2025-01-20 21:07:43 +0700

    Весь 3 модуль

---
+++ 3.7.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._7._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked ((byte) int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age {1}",name,age);
+            Console.Write("What's your favourite day of week?");
+            DayOfWeek number = (DayOfWeek) int.Parse(Console.ReadLine());
+            Console.WriteLine("Your favorite day of week {0}",number);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Enum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jane";
+            int age = 27;
+            string favcolor = "black";
+            Console.WriteLine("{0}\n  {1}\n {2}", name, age, favcolor);
+            Console.ReadKey();
+        }
+    }
+}
